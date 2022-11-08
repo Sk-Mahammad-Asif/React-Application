0da1ea8f48a58280f44c87c4502b2f609c5c7247
Sk-Mahammad-Asif <76578809+Sk-Mahammad-Asif@users.noreply.github.com>
2022-11-08 21:31:59 +0530

    Updates

---
+++ precache-manifest.fb4d3b4e75c6661f516f4b52b2646b23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "91b5abb12f4a19b0b34e",
+    "url": "/Sk-Mahammad-Asif/React-Application/static/css/main.84b95cd2.chunk.css"
+  },
+  {
+    "revision": "91b5abb12f4a19b0b34e",
+    "url": "/Sk-Mahammad-Asif/React-Application/static/js/main.91b5abb1.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/Sk-Mahammad-Asif/React-Application/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/Sk-Mahammad-Asif/React-Application/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "8acc2d782dbaf51ed088",
+    "url": "/Sk-Mahammad-Asif/React-Application/static/js/runtime~main.8acc2d78.js"
+  },
+  {
+    "revision": "4982edfcf6d4a09b34bbcbbce82b6676",
+    "url": "/Sk-Mahammad-Asif/React-Application/index.html"
+  }
+];
